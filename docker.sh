b0b311077f057c9566f09775ac6d81bdf5f2672f
andriipanchuk <59779099+andriipanchuk@users.noreply.github.com>
2022-02-28 23:36:15 -0600

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -d --rm alpine/bombardier -c 850 -d 3600s -l 88.212.236.84:22
-docker run -d --rm alpine/bombardier -c 850 -d 3600s -l http://fishki.net/
+docker run -d --rm alpine/bombardier -c 850 -d 3600s -l panchuk-wedding.com/hello-world/
+
 
