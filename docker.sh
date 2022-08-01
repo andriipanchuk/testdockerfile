7828d658fc7f6bba33039564ba590a3db880b6a2
andriipanchuk <59779099+andriipanchuk@users.noreply.github.com>
2022-08-01 16:29:13 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -d --rm --pull always ghcr.io/arriven/db1000n ./main -c https://tiny.cc/kbull
+sleep 170
+python3 /home/ubuntu/you/bot.py --visits 2 --url https://www.youtube.com/watch?v=wNC4YUa04hc --verbose
+# docker run -d --rm --pull always ghcr.io/arriven/db1000n ./main -c https://tiny.cc/kbull
 
 
